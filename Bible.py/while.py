3c30c0f595e7efdeaeca0a9120800dbcc3ff5985
ConradMar3 <conradmare890317@gmail.com>
2022-03-01 20:27:11 +0200

    2022/02/27

--- Bible.py/while.py
+++ Bible.py/while.py
@@ -1,6 +1,4 @@
-number = 1
-
-while number <= 1500:
-    if number % 2 != 0:
-        print(number)
-    number = number +1
+L = []
+while len(L) < 3:
+    new_name = input("Add new name:").strip().capitalize()
+    L.append(new_name)
